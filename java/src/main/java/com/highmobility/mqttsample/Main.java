d43f5f7e9e3857221c797bf96d82b067646033f1
tonis <tonisives@gmail.com>
2022-09-05 08:49:54 +0300

    implement connecting and subscribing to a topic

--- java/src/main/java/com/highmobility/mqttsample/Main.java
+++ java/src/main/java/com/highmobility/mqttsample/Main.java
@@ -1,6 +1,11 @@
 package com.highmobility.mqttsample;
 
+import com.amazonaws.services.iot.client.AWSIotException;
+import com.amazonaws.services.iot.client.AWSIotMessage;
 import com.amazonaws.services.iot.client.AWSIotMqttClient;
+import com.amazonaws.services.iot.client.AWSIotQos;
+import com.amazonaws.services.iot.client.AWSIotTimeoutException;
+import com.amazonaws.services.iot.client.AWSIotTopic;
 import com.highmobility.mqttsample.sampleUtil.KeyStorePasswordPair;
 import com.highmobility.mqttsample.sampleUtil.SampleUtil;
 
@@ -9,21 +14,48 @@ import org.slf4j.LoggerFactory;
 
 
 class Main {
-  Logger logger = LoggerFactory.getLogger("charon.mqtt");
-  KeyStorePasswordPair pair = SampleUtil.getKeyStorePasswordPair(Env.MQTT_SERVICE_CERT, Env.MQTT_SERVICE_PRIVATE_KEY);
+  private static final long PUBLISH_TIMEOUT = 10000L;
+  private static final long CONNECT_TIMEOUT = 10000L;
 
-  AWSIotMqttClient client = new AWSIotMqttClient(
-    Env.MQTT_CLIENT_ENDPOINT,
-    Env.MQTT_CLIENT_ID,
-    pair.getKeyStore(),
-    pair.getKeyPassword()
-  );
+  private final Logger logger = LoggerFactory.getLogger("charon.mqtt");
+  private final KeyStorePasswordPair pair = SampleUtil.getKeyStorePasswordPair(Env.MQTT_SERVICE_CERT, Env.MQTT_SERVICE_PRIVATE_KEY);
 
   public static void main(String[] args) {
     new Main().start();
   }
 
-  public void start() {
+  void start() {
     logger.debug("init MQTT");
+
+    if (pair == null) {
+      logger.debug("Cannot load certificate");
+      return;
+    }
+
+    final AWSIotMqttClient client = new AWSIotMqttClient(
+      Env.MQTT_CLIENT_ENDPOINT,
+      Env.MQTT_CLIENT_ID,
+      pair.getKeyStore(),
+      pair.getKeyPassword()
+    );
+
+    try {
+      client.connect(CONNECT_TIMEOUT);
+      var topic = "live/level13/A249D7040CB1E6190002728F/#";
+      logger.debug("subscribing to %s".formatted(topic));
+      client.subscribe(new CarStreamTopic(topic));
+    } catch (AWSIotException | AWSIotTimeoutException e) {
+      logger.debug(e.getLocalizedMessage());
+    }
+  }
+
+  class CarStreamTopic extends AWSIotTopic {
+    public CarStreamTopic(String topic) {
+      super(topic, AWSIotQos.QOS1);
+    }
+
+    @Override public void onMessage(AWSIotMessage message) {
+      logger.debug("Incoming message %s: %s".formatted(message.getTopic(), message.getStringPayload()));
+    }
   }
 }
